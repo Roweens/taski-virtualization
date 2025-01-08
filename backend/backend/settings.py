7462609b4ab1b9ce278a2385b97fb27de3c4224c
Roweens <enderman2011@gmail.com>
2025-01-08 18:59:46 +0300

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -61,10 +61,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'taski_db',
+        'USER': 'taski_user',
+        'PASSWORD': 'taski_password',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
